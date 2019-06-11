3adb431629522777ae7bf06bb1639e9ae0f3b67e
JeanBilheux <bilheuxjm@ornl.gov>
2019-06-11 11:03:04 -0400

    First commit

---
+++ src/main/java/my/vaadin/app/GridTutorialApplication.java
@@ -0,0 +1,13 @@
+package my.vaadin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridTutorialApplication.class, args);
+	}
+
+}
