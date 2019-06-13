4c26ab92d76614a4226ea79b1615d24e1dd9d819
JeanBilheux <bilheuxjm@ornl.gov>
2019-06-13 12:28:12 -0400

    Started to add button

--- src/main/java/my/vaadin/app/MyUI.java
+++ src/main/java/my/vaadin/app/MyUI.java
@@ -7,19 +7,25 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 @Theme("mytheme")
 @SpringUI
@@ -28,24 +34,34 @@ public class MyUI extends UI {
 
 	private CustomerService service = CustomerService.getInstance();
 	private Grid<Customer> grid = new Grid<>(Customer.class);
-	
-	
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        layout.addComponents(grid);
-        updateList();        
-        setContent(layout);
-    }
+	private TextField filterText = new TextField();
 
-	private void updateList() {
-		List<Customer> customers = service.findAll();
-        grid.setItems(customers);
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+
+		filterText.setCaption("Filter by name...");
+//		filterText.setPlaceholder("This is the placeholder");
+		filterText.addValueChangeListener(e -> updateList());
+		filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+		Button clearFilterTextBtn = new Button(VaadinIcons.CLOSE);
+		clearFilterTextBtn.setDescription("Clear the current filter");
+		clearFilterTextBtn.addClickListener(e->filterText.clear());
+		
+		layout.addComponents(grid);
+		updateList();
+		setContent(layout);
 	}
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+	private void updateList() {
+		List<Customer> customers = service.findAll(filterText.getValue());
+		grid.setItems(customers);
+		grid.setColumns("firstName", "lastName", "email");
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
 }
